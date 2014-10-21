3c035133178daaa458ffe865f8650f9e0e20516d
free <free@free-VAIO>
2014-10-20 23:36:10 -0200

    correção identação

--- testeMvc/src/br/com/rachacontas/entidades/Usuario.java
+++ testeMvc/src/br/com/rachacontas/entidades/Usuario.java
@@ -22,88 +22,105 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Table(name = "spring_usuario", schema = "DBA")
 public class Usuario implements java.io.Serializable {
 
+	@NotNull
+	@Column(name = "data_cadastro", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCadastro = new Date();
+
+	@Email(message = "Isto não é e-mail")
+	@NotNull
+	@NotEmpty
+	@Column(name = "email", nullable = false, length = 128, unique = true)
+	private String email;
+
+
+	@Column(name = "hash_senha", nullable = false, length = 128)
+	private String hashSenha;
+
 	@Id
 	@Generated(GenerationTime.INSERT)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", unique = true)
 	protected long id;
 
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long valor) {
-		this.id = valor;
-	}
-
-	@NotNull
-	@NotEmpty
-	@Column(name = "nome", nullable = false, length = 128)
-	private String nome;
-	
-	@Email(message = "Isto não é e-mail")
-	@NotNull
-	@NotEmpty
-	@Column(name = "email", nullable = false, length = 128, unique = true)
-	private String email;
-	
-	@NotNull
-	@Column(name = "data_cadastro", nullable = false)
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date dataCadastro = new Date();
-	
 	@NotNull
 	@NotEmpty
 	@Size(min = 8, max = 32, message = "Login muito curto ou muito longo")
 	@Column(name = "login", nullable = false, unique = true, length = 64)
 	private String login;
-	
+
+	@NotNull
+	@NotEmpty
+	@Column(name = "nome", nullable = false, length = 128)
+	private String nome;
+
+	private transient String senha;
+
 	@Column(name = "twitter", nullable = true, length = 64, unique = true)
 	private String twitter;
 
-	private transient String senha;
-	
 	@Column(name = "ultimo_login", nullable = true)
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date ultimoLogin;
 
-	@Column(name = "hash_senha", nullable = false, length = 128)
-	private String hashSenha;
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public String getEmail() {
+		return email;
+	}
 
 	public String getHashSenha() {
 		return hashSenha;
 	}
 
-	public void setHashSenha(String valor) {
-		hashSenha = valor;
-	}
-
-	public Date getUltimoLogin() {
-		return ultimoLogin;
-	}
-
-	public void setUltimoLogin(Date data) {
-		ultimoLogin = data;
-	}
-
-	public String getTwitter() {
-		return twitter;
-	}
-
-	public void setTwitter(String valor) {
-		twitter = valor;
+	public long getId() {
+		return id;
 	}
 
 	public String getLogin() {
 		return login;
 	}
 
-	public void setLogin(String login) {
-		this.login = login;
+	public String getNome() {
+		return nome;
 	}
 
 	public String getSenha() {
 		return senha;
+	}
+
+	public String getTwitter() {
+		return twitter;
+	}
+
+	public Date getUltimoLogin() {
+		return ultimoLogin;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setHashSenha(String valor) {
+		hashSenha = valor;
+	}
+
+	public void setId(long valor) {
+		this.id = valor;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public void setSenha(String senha) {
@@ -112,28 +129,12 @@ public class Usuario implements java.io.Serializable {
 		this.senha = senha;
 	}
 
-	public String getNome() {
-		return nome;
+	public void setTwitter(String valor) {
+		twitter = valor;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Date getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(Date dataCadastro) {
-		this.dataCadastro = dataCadastro;
+	public void setUltimoLogin(Date data) {
+		ultimoLogin = data;
 	}
 
 }
